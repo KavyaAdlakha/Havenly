b05cfeed4194db5875f56bece6cb3590cbfdaf9d
KavyaAdlakha <kavye14@gmail.com>
2025-08-24 15:33:16 +0530

    DB establish

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require ("mongoose");
-const Listing = require("../Havenly/models/listing")
+const Listing = require("../Havenly/models/listing");
+const path = require("path");   
+const methodOverride = require("method-override")
 
 main().then(() => {
     console.log("connected to DB")
@@ -14,23 +16,83 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/havenly")
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
 app.get("/", (req,res) => {
     res.send("hello")
 });
 
-app.get("/testListing", async (req,res) => {
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the lake",
-        price: 1200,
-        location: "Calangute Goa",
-        country: "India"
-    });
+// app.get("/testListing", async (req,res) => {
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the lake",
+//         price: 1200,
+//         location: "Calangute Goa",
+//         country: "India"
+//     });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing")
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing")
+// });
+
+//index route------------------------------>
+
+app.get("/listings", async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+})
+
+//new route----------------------------->
+
+app.get("/listings/new", (req,res) => {
+    res.render("listings/new.ejs")
+})
+
+//create route-------------------------->
+
+app.post("/listings", async (req,res) => {
+    // let {title, description, image, price, location, country} = req.body;
+    const newListing  = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+//show routee------------------------>
+
+app.get("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing})
+})
+
+//edit route---------------------------->
+
+app.get("/listings/:id/edit", async (req,res) =>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing})
 });
+
+//update route------------------------------>
+
+app.put("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//delete route---------------------------->
+
+app.delete("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    console.log(deleteListing)
+    res.redirect("/listings");
+})
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
