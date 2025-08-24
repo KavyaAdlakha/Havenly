9660cfa60c6d4404f4547f5a417983e4183818ab
KavyaAdlakha <kavye14@gmail.com>
2025-08-24 17:32:25 +0530

    ejs-mate part

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require ("mongoose");
 const Listing = require("../Havenly/models/listing");
 const path = require("path");   
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 main().then(() => {
     console.log("connected to DB")
@@ -20,6 +21,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req,res) => {
     res.send("hello")
