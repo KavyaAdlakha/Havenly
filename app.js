34f3098783f206bd11bcab7ad48ee20b3ddc17ab
KavyaAdlakha <kavye14@gmail.com>
2025-08-26 18:25:45 +0530

    server side validation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("../Havenly/models/listing");
 const path = require("path");   
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils(extra)/wrapAsync.js")
+const ExpressError = require("./utils(extra)/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 main().then(() => {
     console.log("connected to DB")
@@ -42,12 +45,23 @@ app.get("/", (req,res) => {
 //     res.send("successful testing")
 // });
 
+const validatelisting = (req,res,next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();
+    }
+}
+
 //index route------------------------------>
 
-app.get("/listings", async (req,res) => {
+app.get("/listings", wrapAsync(async (req,res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
 })
+);
 
 //new route----------------------------->
 
@@ -57,44 +71,62 @@ app.get("/listings/new", (req,res) => {
 
 //create route-------------------------->
 
-app.post("/listings", async (req,res) => {
+app.post("/listings", validatelisting,  wrapAsync(async (req,res,next) => {
     // let {title, description, image, price, location, country} = req.body;
-    const newListing  = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+        const newListing  = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
 })
+);
 
 //show routee------------------------>
 
-app.get("/listings/:id", async (req,res) => {
+app.get("/listings/:id", wrapAsync(async (req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing})
 })
+);
 
 //edit route---------------------------->
 
-app.get("/listings/:id/edit", async (req,res) =>{
+app.get("/listings/:id/edit", wrapAsync(async (req,res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing})
-});
+})
+);
 
 //update route------------------------------>
 
-app.put("/listings/:id", async (req,res) => {
+app.put("/listings/:id", validatelisting, wrapAsync(async (req,res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
 })
+);
 
 //delete route---------------------------->
 
-app.delete("/listings/:id", async (req,res) => {
+app.delete("/listings/:id", wrapAsync(async (req,res) => {
     let {id} = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing)
     res.redirect("/listings");
+})
+);
+
+//error handling to random all pages othe rthan websites 
+app.use((req,res,next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+
+// custom error handling middleware
+
+app.use((err,req,res,next) => {
+    let {status=500, message="Something Went Wrong"} = err;
+    res.status(status).render("error.ejs", {err});
 })
 
 app.listen(8080, () => {
