6516f0ded08e1cbfbd7cb0f92036c66b317cdd58
KavyaAdlakha <kavye14@gmail.com>
2025-08-29 11:58:28 +0530

    Express router

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require ("mongoose");
-const Listing = require("../Havenly/models/listing");
 const path = require("path");   
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils(extra)/wrapAsync.js")
-const ExpressError = require("./utils(extra)/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("../Havenly/models/review.js");
+const ExpressError = require("./utils(extra)/ExpressError.js"); 
+
+const listings = require("./routes/listing.js")
+const reviews = require("./routes/review.js")
 
 main().then(() => {
     console.log("connected to DB")
@@ -32,122 +31,15 @@ app.get("/", (req,res) => {
     res.send("hello")
 });
 
-// app.get("/testListing", async (req,res) => {
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the lake",
-//         price: 1200,
-//         location: "Calangute Goa",
-//         country: "India"
-//     });
+app.use("/listings", listings);
 
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("successful testing")
-// });
+app.use("/listings/:id/reviews", reviews)
 
-const validatelisting = (req,res,next) => {
-    let {error} = listingSchema.validate(req.body);
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else{
-        next();
-    }
-};
-
-const validatereview = (req,res,next) => {
-    let {error} = reviewSchema.validate(req.body);
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else{
-        next();
-    }
-};
-
-//index route------------------------------>
-
-app.get("/listings", wrapAsync(async (req,res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-})
-);
-
-//new route----------------------------->
-
-app.get("/listings/new", (req,res) => {
-    res.render("listings/new.ejs")
-})
-
-//create route-------------------------->
-
-app.post("/listings", validatelisting,  wrapAsync(async (req,res,next) => {
-    // let {title, description, image, price, location, country} = req.body;
-        const newListing  = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-})
-);
-
-//show routee------------------------>
-
-app.get("/listings/:id", wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", {listing})
-})
-);
-
-//edit route---------------------------->
-
-app.get("/listings/:id/edit", wrapAsync(async (req,res) =>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing})
-})
-);
-
-//update route------------------------------>
-
-app.put("/listings/:id", validatelisting, wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-})
-);
-
-//delete route---------------------------->
-
-app.delete("/listings/:id", wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    let deleteListing = await Listing.findByIdAndDelete(id);
-    console.log(deleteListing)
-    res.redirect("/listings");
-})
-);
-
-//Reviews  POST ROUTE----------------------------->
-
-app.post("/listings/:id/reviews", validatereview, wrapAsync( async (req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    console.log("new review saved")
-     res.redirect(`/listings/${listing._id}`);
-})
-);
 
 //error handling to random all pages othe rthan websites 
 app.use((req,res,next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
-
 
 // custom error handling middleware
 
